b55f8ff3d160e20507a2fff32091ecd570cf89f2
hieuxeko19991 <hieuxeko1999@gmail.com>
2021-10-17 17:27:53 +0700

    [BLOG_API_1]

---
+++ endpoints/blog/blog.go
@@ -0,0 +1,108 @@
+package account
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"gitlab.com/hieuxeko19991/job4e_be/services/account"
+	"gitlab.com/hieuxeko19991/job4e_be/services/blog"
+
+	"github.com/gin-gonic/gin"
+	"gitlab.com/hieuxeko19991/job4e_be/pkg/ginx"
+	"gitlab.com/hieuxeko19991/job4e_be/pkg/models"
+	"go.uber.org/zap"
+)
+
+const cookieName = "blog"
+
+type BlogSerializer struct {
+	blogService blog.IBlogService
+	Logger      *zap.Logger
+}
+
+func NewAccountSerializer(accountService account.IAccountService, logger *zap.Logger) *AccountSerializer {
+	return &AccountSerializer{
+		accountService: accountService,
+		Logger:         logger,
+	}
+}
+
+func (as *AccountSerializer) Login(ginCtx *gin.Context) {
+	ctx := ginCtx.Request.Context()
+	req := models.RequestLogin{}
+	err := json.NewDecoder(ginCtx.Request.Body).Decode(&req)
+	if err != nil {
+		as.Logger.Error("Parse request Login error", zap.Error(err))
+		ginx.BuildErrorResponse(ginCtx, err, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	as.Logger.Info("Request", zap.Reflect("request", req))
+	token, err := as.accountService.Login(ctx, req.Email, req.Password)
+	if err != nil {
+		as.Logger.Error("Login error", zap.Error(err))
+		ginx.BuildErrorResponse(ginCtx, err, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	ginCtx.SetCookie(cookieName, token, time.Now().Add(time.Hour*24).Second(), "/", "", true, true)
+
+	ginx.BuildSuccessResponse(ginCtx, http.StatusAccepted, gin.H{
+		"token": token,
+	})
+}
+
+func (as *AccountSerializer) Logout(ginCtx *gin.Context) {
+	ginCtx.SetCookie(cookieName, "", -1, "/", "", true, true)
+	ginx.BuildSuccessResponse(ginCtx, http.StatusOK, nil)
+}
+
+func (as *AccountSerializer) Register(ginCtx *gin.Context) {
+	ctx := ginCtx.Request.Context()
+	req := models.RequestRegisterAccount{}
+	err := json.NewDecoder(ginCtx.Request.Body).Decode(&req)
+
+	if err != nil {
+		as.Logger.Error("Parse request Register account error", zap.Error(err))
+		ginx.BuildErrorResponse(ginCtx, err, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	err = as.accountService.Register(ctx, &req)
+	if err != nil {
+		as.Logger.Error("Register account error", zap.Error(err))
+		ginx.BuildErrorResponse(ginCtx, err, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	ginx.BuildSuccessResponse(ginCtx, http.StatusAccepted, nil)
+}
+
+func (as *AccountSerializer) GetUserFromCookie(ginCtx *gin.Context) {
+	ctx := ginCtx.Request.Context()
+	cookie, err := ginCtx.Cookie(cookieName)
+	if err != nil {
+		ginx.BuildErrorResponse(ginCtx, err, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	acc, err := as.accountService.GetUserFromCookie(ctx, cookie)
+
+	if err != nil {
+		ginx.BuildErrorResponse(ginCtx, err, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	ginx.BuildSuccessResponse(ginCtx, http.StatusOK, acc)
+}
---
+++ services/blog/blog.go
@@ -0,0 +1,113 @@
+package blog
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/pkg/errors"
+	"gitlab.com/hieuxeko19991/job4e_be/pkg/models"
+	"go.uber.org/zap"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type IBlogService interface {
+	Login(ctx context.Context, email string, password string) (string, error)
+	Logout(ctx context.Context, email string) error
+	Register(ctx context.Context, account *models.RequestRegisterAccount) error
+	GetUserFromCookie(ctx context.Context, cookie string) (*models.Account, error)
+}
+
+type IAccountDatabase interface {
+	Create(ctx context.Context, account *models.Account) error
+	GetAccountByEmail(ctx context.Context, email string) (*models.Account, error)
+	GetAccountByID(ctx context.Context, id string) (*models.Account, error)
+}
+
+type Account struct {
+	AccountGorm *AccountGorm
+	SecretKey   string
+	Logger      *zap.Logger
+}
+
+func NewAccount(accountGorm *AccountGorm, secretKey string, logger *zap.Logger) *Account {
+	return &Account{
+		AccountGorm: accountGorm,
+		SecretKey:   secretKey,
+		Logger:      logger,
+	}
+}
+
+func (a *Account) Login(ctx context.Context, email string, password string) (string, error) {
+	acc, err := a.AccountGorm.GetAccountByEmail(ctx, email)
+	if err != nil {
+		return "", err
+	}
+
+	if acc.Id == 0 {
+		a.Logger.Error("Account by email not found")
+		return "", errors.New("Account not found")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(acc.Password), []byte(password))
+	if err != nil {
+		a.Logger.Error("Incorrect password")
+		return "", errors.Wrap(err, "Incorrect password")
+	}
+	jsonAccount, err := json.Marshal(acc)
+	if err != nil {
+		a.Logger.Error("Can not convert account to json", zap.Error(err))
+		return "", err
+	}
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    string(jsonAccount),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(), //1 day
+	})
+
+	token, err := claims.SignedString([]byte(a.SecretKey))
+	if err != nil {
+		a.Logger.Error("Cannot get token", zap.Error(err))
+		return "", err
+	}
+
+	return token, nil
+}
+
+func (a *Account) Logout(ctx context.Context, email string) error {
+	return nil
+}
+
+func (a *Account) Register(ctx context.Context, account *models.RequestRegisterAccount) error {
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(account.Password), 8)
+	accountModels := &models.Account{
+		Email:    account.Email,
+		Phone:    account.Phone,
+		Password: string(hashedPassword),
+		Type:     account.Type,
+	}
+	err := a.AccountGorm.Create(ctx, accountModels)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *Account) GetUserFromCookie(ctx context.Context, cookie string) (*models.Account, error) {
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(a.SecretKey), nil
+	})
+	if err != nil {
+		a.Logger.Error("Unauthenticated!!", zap.Error(err))
+		return nil, err
+	}
+
+	claims := token.Claims.(*jwt.StandardClaims)
+
+	acc, err := a.AccountGorm.GetAccountByID(ctx, claims.Issuer)
+	if err != nil {
+		a.Logger.Error("Can not get Account by ID", zap.Error(err))
+		return nil, err
+	}
+	return acc, nil
+}
