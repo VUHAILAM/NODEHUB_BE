896ff516a7dc10b880c2713ac167f140c2162a86
lamvh3 <lam.vu@onemount.com>
2021-10-25 01:34:31 +0700

    add file key

--- migrates/create_setting_table.sql
+++ migrates/create_setting_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `setting`
 (
     `setting_id` int NOT NULL AUTO_INCREMENT,
-    `type` varchar(255) NOT NULL UNIQUE,
+    `type` varchar(255) NOT NULL,
     `name` varchar(255) NOT NULL,
     `created_at`  datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at`  datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
