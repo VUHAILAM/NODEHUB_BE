a22ca315474450f894c0d876fd6da7bfca8fcec7
lamvh3 <lam.vu@onemount.com>
2021-12-05 22:46:23 +0700

    unit test

--- services/skill/skill_test.go
+++ services/skill/skill_test.go
@@ -1,1 +1,31 @@
 package skill
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+
+	"gitlab.com/hieuxeko19991/job4e_be/pkg/models"
+)
+
+type MockSkillGorm struct {
+	mock.Mock
+}
+
+func (m *MockSkillGorm) Create(ctx context.Context, skill *models.Skill) error {
+	args := m.Called(ctx, skill)
+
+}
+
+func TestSkill_CreateSkill(t *testing.T) {
+	testcases := []struct {
+		Name          string
+		Req           *models.RequestCreateSkill
+		MockObject    Skill
+		ExpectedError error
+	}{
+		{},
+	}
+
+}
